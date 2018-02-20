d4f5445b58c8ab048da18762bd2614d43d2ab7d9
Son Tran <sonempty@gmail.com>
2018-02-20 16:16:32 +0700

    update

--- dataservices/binance.js
+++ dataservices/binance.js
@@ -3,6 +3,14 @@
 // Require dependencies
 const axios = require('axios');
 const events = require('events');
+const redis = require('redis');
+
+// create a new redis client and connect to our local redis instance
+var client = redis.createClient();
+client.on('error', function(err) {
+  console.log("Redis Error " + err);
+});
+
 
 // Binance Exchange Base API URL
 const BINANCE_BASE_URL = 'https://api.binance.com/api/v1/';
@@ -17,17 +25,19 @@ eventEmitter.on('binance_tickers', initOCLH);
 
 //Init <limit> records for each interval and coin
 async function initOCLH(symbols) {
-  /* Delete exist records and creat new one firstly*/
   for (let interval of ['15m', '1h', '4h', '1d']) {
     for (let symbol of symbols) {
       let limit = 2;
       let url = `${ BINANCE_BASE_URL }klines?symbol=${ symbol }&interval=${ interval }&limit=${ limit }`;
-
+      client.del(`binance_${ symbol }_${ interval }`)
       await sleep(500);
       let result = await axios.get(url);
-
-      //Update to Redis Cache - now log to console for testing only
-      console.log(symbol + ' init ' + result.data);
+      let items = result.data
+      items.forEach(item => {
+        //Update to Redis Cache
+        // startTime - startTime, o, c, l, h, vol, basevol
+        client.zadd(`binance_${ symbol }_${ interval }`, item[0], `${ item[0] } ${ item[1] } ${ item[4] } ${ item[3] } ${ item[2] } ${ item[5] } ${ item[7] }`)
+      })
 
       //Call last Update
       getLastOCLH(symbol, interval);
@@ -38,18 +48,25 @@ async function initOCLH(symbols) {
 //Run function `lastOCLH` during time interval
 function getLastOCLH(symbol, interval) {
   let url = `${ BINANCE_BASE_URL }klines?symbol=${ symbol }&interval=${ interval }&limit=1`;
-  let transform_interval = interval == '15m' ? 15*60*1000 : interval == '1h' ? 60*60*1000 : interval == '4h'? 4*60*60*1000 : 24*60*60*1000
-  setInterval( () => {
+  let transform_interval = interval == '15m' ? 15 * 60 * 1000 : interval == '1h' ? 60 * 60 * 1000 : interval == '4h' ? 4 * 60 * 60 * 1000 : 24 * 60 * 60 * 1000
+  setInterval(() => {
     axios.get(url)
-      .then( datas => {
-        console.log(symbol + ' -- ' + transform_interval + '  :' + datas.data[0])
+      .then(datas => {
+        let item = datas.data[0]
+        client.zadd(`binance_${ symbol }_${ interval }`,
+          item[0],
+          `${ item[0] } ${ item[1] } ${ item[4] } ${ item[3] } ${ item[2] } ${ item[5] } ${ item[7] }`,
+        function (err, res) {
+          if(err) console.log('getLastOCLH fail ' + symbol + '  ' + err)
+          if(res) client.ZREMRANGEBYRANK(`binance_${ symbol }_${ interval }`, 0, 0)
+        })
+        console.log(symbol + ' -- ' + transform_interval + ':   ' + datas.data[0])
       })
-  }, transform_interval)
+  }, 1.01 * transform_interval)
 }
 
 //Check and Update Exchanges coin if have new listing/delisting coin
 function getTicker() {
-
   let result = [];
   axios.get(BINANCE_INFOR_URL)
     .then(datas => {
