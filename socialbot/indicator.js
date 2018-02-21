7c5ca023b1f5ff905b67d12268f48496eb700be6
Son Tran <sonempty@gmail.com>
2018-02-21 18:26:30 +0700

    fsd

--- socialbot/indicator.js
+++ socialbot/indicator.js
@@ -31,11 +31,12 @@ function calculateIndicator(err, symbols) {
             let rows = r.split(' ')
 						let [t1, o1, c1, l1, h1, v1, bv1] = rows
 						t.push(+t1)
+						console.log(t1)
           })
 					//let sma5 = SMA.calculate({period : 5, values : c})
 					//let sma10 = SMA.calculate({period : 10, values : c})
 					//let sma20 = SMA.calculate({period : 20, values : c})
-					console.log(`binance_${ symbol }_${ interval }` + ' ' + t)
+					//console.log(`binance_${ symbol }_${ interval }` + ' ' + t)
 					//console.log(symbol + ' Time: ' + t[t.length -1] + ' SMA5: ' + sma5)
 					//console.log(symbol + ' Time: ' + new Date(t[0]).toLocaleString() + ' SMA10: ' + sma10)
 					//console.log(symbol + ' Time: ' + new Date(t[0]).toLocaleString() + ' SMA20: ' + sma20)
